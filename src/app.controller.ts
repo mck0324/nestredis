c5055a30b6060346562d3e42ee591251e0246972
mck0324 <dch3030@naver.com>
2023-12-24 17:04:54 +0900

    local Cache테스트 및 테스트코드 작성

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CacheInterceptor, CacheKey, CacheTTL } from '@nestjs/cache-manager';
 
+
+@UseInterceptors(CacheInterceptor)
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  // @CacheKey('some_route')
+  // @CacheTTL(60)
   @Get()
-  getHello(): string {
+  async getHello() {
     return this.appService.getHello();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
-import { CacheModule } from '@nestjs/cache-manager';
+import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   imports: [CacheModule.register()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,
+    {
+    provide: APP_INTERCEPTOR,
+    useClass: CacheInterceptor,
+  }
+],
 })
 export class AppModule {}
